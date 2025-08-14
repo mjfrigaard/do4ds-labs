fc6669bd988c7247e110484a8cb973ec30323aef
Martin Frigaard <mjfrigaard@pm.me>
2025-08-14 00:38:02 -0700

    updates to app and api (R)

--- _labs/lab3/R/app/app-api.R
+++ _labs/lab3/R/app/app-api.R
@@ -1,41 +1,56 @@
 library(shiny)
+library(bslib)
+library(bsicons)
 library(httr2)
 
 api_url <- "http://127.0.0.1:8080/predict"
 
-ui <- fluidPage(
-  titlePanel("Penguin Mass Predictor"),
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput(
-        inputId = "bill_length", 
-        label = "Bill Length (mm)", 
-        min = 30, 
-        max = 60, 
-        value = 45, 
-        step = 1),
-      selectInput(
-        inputId = "sex", 
-        label = "Sex", 
-        choices = c("Male", "Female"), 
-        selected = "Male"
-        ),
-      selectInput(
-        inputId = "species", 
-        label = "Species", 
-        choices = c("Adelie", "Chinstrap", "Gentoo"),
-        selected = "Adelie"
-        ),
-      actionButton(
-        inputId = "predict",
-        label = "Predict")
+ui <- page_sidebar(
+  title = "Penguin Mass Predictor",
+  theme = bs_theme(bootswatch = "sketchy"),
+  sidebar = sidebar(
+    sliderInput(inputId = "bill_length", 
+      label = "Bill Length (mm)",
+      min = 30, 
+      max = 60, 
+      value = 45, 
+      step = 1),
+    selectInput(inputId = "sex", 
+      label = "Sex", 
+      choices = c("Male", "Female"), 
+      selected = "Male"),
+    selectInput(
+      inputId = "species",
+      label = "Species",
+      choices = c("Adelie", "Chinstrap", "Gentoo"),
+      selected = "Adelie"),
+    actionButton(
+      inputId = "predict", 
+      label = "Predict", 
+      class = "btn-primary")
+  ),
+  
+  layout_columns(
+    card(
+      card_header("Penguin Parameters"),
+      card_body(
+      verbatimTextOutput("vals")
+      )
     ),
-    mainPanel(
-      h2("Penguin Parameters"),
-      verbatimTextOutput(outputId = "vals"),
-      h2("Predicted Penguin Mass (g)"),
-      textOutput(outputId = "pred")
-    )
+    card(
+      card_header("Predicted Mass"),
+      card_body(
+        value_box(
+          showcase_layout = "left center",
+          title = "Grams",
+          value = textOutput("pred"),
+          showcase = bs_icon("graph-up"),
+          max_height = "200px",
+          min_height = "200px",
+        )
+      )
+    ),
+    col_widths = c(7, 5)
   )
 )
 
@@ -65,7 +80,10 @@ server <- function(input, output) {
     round(pred(), 1)
   })
   
-  output$vals <- renderPrint(vals())
+  output$vals <- renderPrint({
+    vals()
+    })
+  
 }
 
 shinyApp(ui = ui, server = server)
