aa2e7af55507ee490d2ca9dced865a3f4db33de2
Martin Frigaard <mjfrigaard@pm.me>
2025-12-16 12:20:55 -0700

    updates to plumber

--- _labs/lab3/R/api/README.md
+++ _labs/lab3/R/api/README.md
@@ -9,3 +9,29 @@
 │       └── 20251004T054448Z-355a4
 └── renv.lock               # dependency management
 ```
+
+## mod-api.R
+
+```r
+
+```
+
+## plumber.R
+
+### handle_ping()
+
+```bash
+curl http://127.0.0.1:8080/ping
+```
+
+### handle_health()
+
+```bash
+curl http://127.0.0.1:8080/health
+```
+
+### handle_model_prototype()
+
+```bash
+curl http://127.0.0.1:8080/model-prototype
+```
--- _labs/lab3/R/api/mod-api.R
+++ _labs/lab3/R/api/mod-api.R
@@ -8,8 +8,10 @@ model_board <- pins::board_folder("../../../lab2/R/models/")
 # read pinned vetiver model
 v <- vetiver::vetiver_pin_read(model_board, "penguin_model")
 
-# use plumber/vetiver to create router/create API
-app <- plumber::pr() |> vetiver::vetiver_api(v)
+# plumber ----
+app <- plumber::pr() |> vetiver::vetiver_api(v) # create plumber API
+app |> plumber::pr_run(port = 8080, host = "127.0.0.1") # run
 
-# run the api
-app |> plumber::pr_run(port = 8080, host = "127.0.0.1")
+# plumber2 ----
+# app <- plumber2::api() |> vetiver::vetiver_api(v) # create plumber2 API
+# app |> plumber2::api_run(port = 8080, host = "127.0.0.1") # run
